abae6ad6369d632cc13ae472f9608ca086620ea0
claire provot <claire.provot@incomlend.com>
2021-02-05 10:24:07 +0800

    init

---
+++ src/org/runnable/LongWrapper.java
@@ -0,0 +1,23 @@
+package org.runnable;
+
+import java.util.Objects;
+
+public class LongWrapper {
+
+    private Object key = new Object();
+    private long l;
+
+    public LongWrapper(long l) {
+        this.l = l;
+    }
+
+    public long getValue() {
+        return l;
+    }
+
+    public void incrementValue() {
+        synchronized (key) {
+            l = l + 1;
+        }
+    }
+}
